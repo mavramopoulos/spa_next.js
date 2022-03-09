d93c188dc040e7fe32bbf9f92aaf8bf3001efdfb
mavramopoulos <mavramopoulos@innovative.gr>
2022-03-09 15:25:33 +0200

    googleAuth

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,15 @@
 import '../styles/globals.css'
-import { config } from '@fortawesome/fontawesome-svg-core'
+import {config} from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
+import {SessionProvider} from "next-auth/react"
+
 config.autoAddCss = false
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}) {
+    return (
+        <SessionProvider session={pageProps.session}>
+            <Component {...pageProps} />
+        </SessionProvider>)
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,32 @@
-import {AppBar, Box, Toolbar, Typography} from "@mui/material";
+import {AppBar, Box, Button, Toolbar, Typography} from "@mui/material";
 import Spa from "./spa";
+import {useSession, signIn, signOut} from "next-auth/react"
 
 export default function Home() {
-  return (
-      <div style={{paddingBottom: 100}}>
-        <Box>
-          <AppBar position="static" style={{background: "black"}}>
-            <Toolbar>
-              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                SPA
-              </Typography>
-            </Toolbar>
-          </AppBar>
-        </Box>
-        <Spa/>
-      </div>
-  )
+    const {data: session} = useSession()
+    if (session) {
+        return (
+            <div style={{paddingBottom: 100}}>
+                <Box>
+                    <AppBar position="static" style={{background: "black"}}>
+                        <Toolbar>
+                            <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
+                                SPA
+                            </Typography>
+                            Signed in as {session.user.email} <br/>
+                            <Button style={{marginLeft: 10}} variant="contained" onClick={() => signOut()}>Sign out</Button>
+                        </Toolbar>
+                    </AppBar>
+                </Box>
+                <Spa/>
+            </div>
+        )
+    } else {
+        return (
+            <div align="center">
+                <h1>You are not Signed in!</h1>
+                <Button variant="contained" onClick={() => signIn()}>Sign in</Button>
+            </div>
+        )
+    }
 }
