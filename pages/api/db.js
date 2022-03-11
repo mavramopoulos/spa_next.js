62c86aa701bc8a748ed727e78134f3c610965e5a
mavramopoulos <mavramopoulos@innovative.gr>
2022-03-11 11:13:03 +0200

    heroku fix

--- pages/api/db.js
+++ pages/api/db.js
@@ -1,19 +1,13 @@
 const getCon = () =>{
     const Pool = require('pg').Pool
 
-    let ssl
 
-    if (process.env.NODE_ENV === 'development') {
-        ssl = {rejectUnauthorized: false};
-    }
 
    return new Pool({
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE,
-        port: process.env.DB_PORT,
-        host: process.env.DB_HOST,
-        ssl: ssl
+       connectionString: process.env.DATABASE_URL,
+       ssl: {
+           rejectUnauthorized: false
+       }
     });
 
 }
