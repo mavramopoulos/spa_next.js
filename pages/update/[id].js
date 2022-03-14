9359cbc36255854f4fa2f1742f7980312f178755
mavramopoulos <mavramopoulos@innovative.gr>
2022-03-14 17:25:10 +0200

    routing

--- pages/update/[id].js
+++ pages/update/[id].js
@@ -11,13 +11,13 @@ import moment from "moment";
 export default function Update() {
     const router = useRouter()
     const {data: session, status} = useSession()
-    const [data, setData] = useState()
+    const [data, setData] = useState([])
     const [lastNameU, setLastNameU] = useState("")
     const [firstNameU, setFirstNameU] = useState("")
     const [isActiveU, setIsActiveU] = useState('')
     const [dateU, setDateU] = useState("")
     const [id, setId] = useState("")
-
+    const [check, setCheck] = useState(false)
 
     async function Fetch() {
         await fetch("../api/get/" + id,
@@ -61,17 +61,23 @@ export default function Update() {
     }, [router]);
 
     useEffect(() => {
-        if (id){
+        if (id && status === "authenticated") {
             Fetch()
         }
-    }, [id]);
+    }, [id, status]);
 
     useEffect(() => {
-        if (data){
-            setLastNameU(data[0].last_name)
-            setFirstNameU(data[0].first_name)
-            setIsActiveU(data[0].is_active)
-            setDateU(moment(data[0].date_of_birth, 'DD/MM/YYYY').format('MM/DD/YYYY'))
+
+        if (id && status === "authenticated") {
+            if (Object.keys(data).length === 1) {
+                setLastNameU(data[0].last_name)
+                setFirstNameU(data[0].first_name)
+                setIsActiveU(data[0].is_active)
+                setDateU(moment(data[0].date_of_birth, 'DD/MM/YYYY').format('MM/DD/YYYY'))
+                setCheck(true)
+            }else{
+                router.push('/view')
+            }
         }
     }, [data]);
 
@@ -81,66 +87,71 @@ export default function Update() {
         }
     }, [status]);
 
-    return (
-        <div style={{marginTop: 100, paddingRight: 100, paddingLeft: 100}}>
-            <h1>Update</h1>
-            <Box
-                sx={{
-                    '& .MuiTextField-root': {m: 1, width: '25ch'},
-                }}
-                noValidate
-                autoComplete="off"
-            >
-                <form onSubmit={handleUpdate}>
-                    <div>
-                        <TextField
+    const Page = () => {
+        return (
+            <div style={{marginTop: 100, paddingRight: 100, paddingLeft: 100, display: check ? 'block' : 'none'}}>
+                <h1>Update</h1>
+                <Box
+                    sx={{
+                        '& .MuiTextField-root': {m: 1, width: '25ch'},
+                    }}
+                    noValidate
+                    autoComplete="off"
+                >
+                    <form onSubmit={handleUpdate}>
+                        <div>
+                            <TextField
 
-                            id="last_nameU"
-                            label="Last Name"
-                            name="last_nameU"
-                            value={lastNameU}
-                            onChange={e => setLastNameU(e.target.value)}
-                        />
-                        <TextField
-
-                            id="first_nameU"
-                            label="First Name"
-                            name="first_nameU"
-                            value={firstNameU}
-                            onChange={e => setFirstNameU(e.target.value)}
-                        />
-                        <Select style={{width: 300, marginTop: 8}}
-
-                                id="is_activeU"
-                                label="Is Active"
-                                name="is_activeU"
-                                value={isActiveU}
-                                onChange={e => setIsActiveU(e.target.value)}
-                        >
-                            <MenuItem value="">
-                                <em>None</em>
-                            </MenuItem>
-                            <MenuItem value="True">Yes</MenuItem>
-                            <MenuItem value="False">No</MenuItem>
-                        </Select>
-                        <LocalizationProvider dateAdapter={AdapterDateFns}>
-                            <DatePicker
-                                id="date_of_birthU"
-                                label="Date Of Birth"
-                                name="date_of_birthU"
-                                value={dateU}
-                                date={null}
-                                onChange={newValue => setDateU(newValue)}
-                                renderInput={(params) => <TextField {...params} />}
-                                inputFormat="dd/MM/yyyy"
+                                id="last_nameU"
+                                label="Last Name"
+                                name="last_nameU"
+                                value={lastNameU}
+                                onChange={e => setLastNameU(e.target.value)}
                             />
-                        </LocalizationProvider>
-                    </div>
-                    <Button style={{marginLeft: 10}} variant="contained" type="submit">Submit</Button>
-                    <Button style={{marginLeft: 10, background: "indianred"}} variant="contained"
-                            onClick={() => router.push('/view')}>Cancel</Button>
-                </form>
-            </Box>
-        </div>
-    )
+                            <TextField
+
+                                id="first_nameU"
+                                label="First Name"
+                                name="first_nameU"
+                                value={firstNameU}
+                                onChange={e => setFirstNameU(e.target.value)}
+                            />
+                            <Select style={{width: 300, marginTop: 8}}
+
+                                    id="is_activeU"
+                                    label="Is Active"
+                                    name="is_activeU"
+                                    value={isActiveU}
+                                    onChange={e => setIsActiveU(e.target.value)}
+                            >
+                                <MenuItem value="">
+                                    <em>None</em>
+                                </MenuItem>
+                                <MenuItem value="True">Yes</MenuItem>
+                                <MenuItem value="False">No</MenuItem>
+                            </Select>
+                            <LocalizationProvider dateAdapter={AdapterDateFns}>
+                                <DatePicker
+                                    id="date_of_birthU"
+                                    label="Date Of Birth"
+                                    name="date_of_birthU"
+                                    value={dateU}
+                                    date={null}
+                                    onChange={newValue => setDateU(newValue)}
+                                    renderInput={(params) => <TextField {...params} />}
+                                    inputFormat="dd/MM/yyyy"
+                                />
+                            </LocalizationProvider>
+                        </div>
+                        <Button style={{marginLeft: 10}} variant="contained" type="submit">Submit</Button>
+                        <Button style={{marginLeft: 10, background: "indianred"}} variant="contained"
+                                onClick={() => router.push('/view')}>Cancel</Button>
+                    </form>
+                </Box>
+            </div>
+        )
+    }
+
+    return <Page />
+
 }
